39553ce4fc7c214564548b3f5f92b8b6c6d33975
Vladislav Simutin <vlad.sn.s0n@gmail.com>
2024-08-02 16:42:41 +0700

    Add new project with OPN payment system and CRM SalesRender integration

---
+++ src/lib/components/InputField.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	export let value: string;
+	export let label: string;
+	export let type: string = 'text';
+
+	function typeAction(node: HTMLInputElement): void {
+		node.type = type;
+	}
+</script>
+
+<p class="form-control">
+	{#if label}
+		<label class="label" for="{label}">{label}:</label>
+	{/if}
+	<input id="{label}" use:typeAction class="input" bind:value={value}/>
+</p>
+
+<style>
+	.form-control {
+		margin: .5rem 0;
+		text-align: left;
+	}
+	.input {
+		width: 100%;
+		display: block;
+		padding: 0.5rem 0;
+		margin-top: 0.5rem;
+		border-width: 1px;
+		border-radius: 0.25rem;
+		background-color: bisque;
+	}
+	.label {
+		color: gray;
+	}
+</style>
